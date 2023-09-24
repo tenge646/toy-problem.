12bb8cb5e64b6a79b87c32fcb2520c1d515d4ebe
kinsman646 <teesam2004@gmail.com>
2023-09-24 15:31:19 +0300

    Created the code well but its not running on my end

---
+++ SpeedDetector/index.js
@@ -0,0 +1,16 @@
+const readline = require("readline");
+const speedDetector = require("../speed");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter the speed of the car (in km/h): ", function (speedInput) {
+  const speed = parseFloat(speedInput);
+
+ 
+  speedDetector(speed);
+
+  rl.close(); 
+});
---
+++ SpeedDetector/speed.js
@@ -0,0 +1,13 @@
+function speedDetector(speed) {
+    const speedLimit = 70;
+    const demeritPoints = Math.floor((speed - speedLimit) / 5);
+  
+    if (speed <= speedLimit) {
+      console.log("Ok");
+    } else if (demeritPoints <= 12) {
+      console.log("Points: " + demeritPoints);
+    } else {
+      console.log("License suspended");
+    }
+  }
+  module.exports = speedDetector;
